bcabcf445469b3066c617b5ec70cd9fadd3cc2cd
kislyakovm <kislyakovm97@gmail.com>
2024-01-15 15:09:40 +0600

    update

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        List<String> atrList = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(file.toPath())) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] str = line.split(" ");
+                atrList.add(str[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(atrList.get(0), Integer.parseInt(atrList.get(1)), atrList.get(2), Long.parseLong(atrList.get(3)));
     }
 }
